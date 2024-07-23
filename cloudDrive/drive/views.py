a13ee1302105d7e2d58ec2776dce25a6c3b51a95
Padam Damai <padamdamai888@gmail.com>
2024-07-23 14:10:50 +0545

    folderuser added in db

--- cloudDrive/drive/views.py
+++ cloudDrive/drive/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render,redirect
 from new_items.models import *
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required
 def home(request):
-    folders = Folder.objects.all().order_by('-id')
+    folders = Folder.objects.filter(folderName=request.user).order_by('-id')
     if request.method == 'POST':
         folder_name = request.POST['folder_name']
         if Folder.objects.filter(folderName = folder_name).exists():
             messages.error(request," folder name already exists")
             return redirect('/')
         else:
-            folder = Folder.objects.create(folderName = folder_name)
+            folder = Folder.objects.create(folderName = folder_name,folderUser = request.user)
             if folder:
                 messages.success(request,'folder created successfully')
                 return redirect('/')
--- cloudDrive/new_items/admin.py
+++ cloudDrive/new_items/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import * 
 # Register your models here.
-admin.site.register(Folder)
+@admin.register(Folder)
+class Adminfolder(admin.ModelAdmin):
+    list_display=('folderName','folderUser')
